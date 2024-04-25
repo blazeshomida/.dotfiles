e171c2906c524aa9d1becba86696cce32a5579f2
Blaze Shomida <131521446+blazeshomida@users.noreply.github.com>
2024-04-25 04:32:54 -1000

    feat(functions/git)!: :sparkles: make find_commit more robust with bat
    - change name from fcommit to find_commit

--- functions/git.sh
+++ functions/git.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 
-function fcommit {
+function find_commit {
   local commit_hash
-  commit_hash=$(git log --oneline | fzf) &&
-    echo "Selected commit: $commit_hash" &&
+  local query=$1
+  commit_hash=$(
+    git log --oneline |
+      grep -v -e "Merge" |
+      fzf --preview-label='[ Git Commit Preview ]' --preview='echo "#️⃣  Commit Hash: {1}\n💬 Commit Message:{2..}\n📄 Files Changed:\n$(git diff --name-only {1})\n" && git diff {1} | bat --color="always" --style="changes"' --with-nth='2..' --preview-window='up' --query="$query"
+  ) &&
+    echo "Selected commit: $commit_hash" | sed -Ee 's/ :[a-zA-Z]+://g' &&
     echo "$commit_hash" | awk '{print $1}' | pbcopy
 }
