577cbbce36ce58a3c93182071e073633dc4c4586
Blaze Shomida <131521446+blazeshomida@users.noreply.github.com>
2024-04-25 05:26:16 -1000

    feat(enhance/git): :sparkles: enhace find_commit by combining diff and show

--- functions/git.sh
+++ functions/git.sh
@@ -6,7 +6,7 @@ function find_commit {
   commit_hash=$(
     git log --oneline |
       grep -v -e "Merge" |
-      fzf --preview-label='[ Git Commit Preview ]' --preview='echo "#️⃣  Commit Hash: {1}\n💬 Commit Message:{2..}\n📄 Files Changed:\n$(git diff --name-only {1})\n" && git diff {1} | bat --color="always" --style="changes"' --with-nth='2..' --preview-window='up' --query="$query"
+      fzf --preview-label='[ Git Commit Preview ]' --preview='git show {1} --summary | bat --color="always" --style="changes" && git diff {1}  | bat --color="always" --style="changes"' --with-nth='2..' --preview-window='up' --query="$query"
   ) &&
     echo "Selected commit: $commit_hash" | sed -Ee 's/ :[a-zA-Z]+://g' &&
     echo "$commit_hash" | awk '{print $1}' | pbcopy
