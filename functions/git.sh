6afe23fb98c55e13eb930ef579449388090917aa
Blaze Shomida <131521446+blazeshomida@users.noreply.github.com>
2024-04-25 06:28:55 -1000

    feat(enhance/git): :sparkles: enhance find_commit with colored diff and summary outputs
    - `git show`, shows correct diff output
    - show diff separate fromsummary with --format=

--- functions/git.sh
+++ functions/git.sh
@@ -6,7 +6,7 @@ function find_commit {
   commit_hash=$(
     git log --oneline |
       grep -v -e "Merge" |
-      fzf --preview-label='[ Git Commit Preview ]' --preview='git show {1} --summary | bat --color="always" --style="changes" && git diff {1}  | bat --color="always" --style="changes"' --with-nth='2..' --preview-window='up' --query="$query"
+      fzf --preview-label='[ Git Commit Preview ]' --preview='git show {1} --summary | bat --color="always" --style="changes" && git show --format= {1} | grep -v -E "^(diff --git a|---|\+\+\+|index)" | bat --color="always" --style="changes" --language="diff"' --with-nth='2..' --preview-window='up' --query="$query"
   ) &&
     echo "Selected commit: $commit_hash" | sed -Ee 's/ :[a-zA-Z]+://g' &&
     echo "$commit_hash" | awk '{print $1}' | pbcopy
