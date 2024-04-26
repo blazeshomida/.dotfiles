7d2c47d2b06b929505b47802c937f791f463e370
Blaze Shomida <131521446+blazeshomida@users.noreply.github.com>
2024-04-25 18:44:40 -1000

    feat(find_commit): :sparkles: add diff-so-fancy for better diff previews

--- functions/git.sh
+++ functions/git.sh
@@ -4,9 +4,15 @@ function find_commit {
   local commit_hash
   local query=$1
   commit_hash=$(
-    git log --oneline |
-      grep -v -e "Merge" |
-      fzf --preview-label='[ Git Commit Preview ]' --preview='git show {1} --summary | bat --color="always" --style="changes" && git show --format= {1} | grep -v -E "^(diff --git a|---|\+\+\+|index)" | bat --color="always" --style="changes" --language="diff"' --with-nth='2..' --preview-window='right' --query="$query"
+    git log --oneline --color="always" --format="%C(magenta)%h %C(cyan)%s" |
+      fzf --ansi --nth="2.." --query="$query" \
+        --cycle --prompt='Search for a commit: ' \
+        --padding='2' \
+        --preview-label='[ Git Commit Preview ]' \
+        --preview='git show {1} --no-patch | 
+            bat --color="always" --style="grid,snip" && 
+            git show {1} --format="" | 
+            diff-so-fancy'
   ) &&
     echo "Selected commit: $commit_hash" | sed -Ee 's/ :[a-zA-Z]+://g' &&
     echo "$commit_hash" | awk '{print $1}' | pbcopy
