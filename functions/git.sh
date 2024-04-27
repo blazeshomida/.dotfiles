e7bcdda54a80a50367b78d54314421ffcdc95821
Blaze Shomida <131521446+blazeshomida@users.noreply.github.com>
2024-04-26 18:57:38 -1000

    feat(find_commit): :sparkles: enhance find_commit output
    - implement with awk instead of grep & sed

--- functions/git.sh
+++ functions/git.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
 
-# This function allows the user to interactively search and select a git commit from the log.
-# The selected commit hash is displayed and copied to the clipboard.
+# Dependencies:
+# - fzf: A command-line fuzzy finder used for interactive search and selection.
+# - bat: A cat clone with syntax highlighting and Git integration, used for previewing commit details.
+# - diff-so-fancy: A human-readable diff formatter for Git, enhancing the visual presentation of changes.
+
+# Description:
+# This script streamlines the process of searching for a specific commit within a Git repository's history.
+# It leverages fzf to provide an interactive search interface, allowing users to navigate through commit logs efficiently.
+# Additionally, bat is employed to display a preview of the selected commit's details, enhancing readability with syntax highlighting.
+# The diff-so-fancy tool further enhances the visualization of commit changes. With this script,
+# users can seamlessly find and inspect commits, improving their Git workflow.
 function find_commit {
   local commit_hash
   commit_hash=$(
@@ -10,13 +19,17 @@ function find_commit {
         --cycle --no-sort --prompt='Search for a commit: ' \
         --padding='2' \
         --preview-label='[ Git Commit Preview ]' \
-        --preview='git show --no-patch {1} | 
-            grep -v -E "^(commit|Author|Date|Merge:)" | 
-            sed -e "s/^[[:space:]]*//; /^$/d" |
-            bat --color="always" --style="grid,snip" && 
-            git show --format="" {1} | 
-            diff-so-fancy'
+        --preview="git show --no-patch {1} | 
+          awk 'NR > 1 {
+            if (NR >= 6) { gsub(/^[[:blank:]]+|[[:blank:]]+$/,\"\"); print }
+            if (NR < 6 && !/^(Merge|Author|Date)/ && !/^[[:blank:]]*$/) { 
+              gsub(/^[[:blank:]]+|[[:blank:]]+$/,\"\"); print
+            }
+          }' | 
+          bat --style='plain' --color='always' --language='COMMIT_EDITMSG' && 
+          git show --format='' {1} | 
+          diff-so-fancy"
   ) &&
-    echo "Selected commit: $commit_hash" | sed -Ee 's/ :[a-zA-Z]+://g' &&
+    echo "Selected commit: $commit_hash" | sed -E 's/ :[a-zA-Z]+://g' &&
     echo "$commit_hash" | awk '{print $1}' | pbcopy
 }
