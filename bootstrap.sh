aa42e6182fa8f28145f4efb44ff52af5007b64a0
Blaze Shomida <131521446+blazeshomida@users.noreply.github.com>
2024-04-27 17:00:21 -1000

    refactor(bootstrap): :recycle: encapsulate logic into separate functions
    - fixed function declaration consistency

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 DOTFILES_DIR="$HOME/.dotfiles"
 
 # Function to create symbolic links
-function create_symlink {
+function create_symlink() {
     # Get the source and destination paths
     local source=$1
     local destination=$2
@@ -51,13 +51,14 @@ function create_symlink {
     fi
 }
 
-function is_valid_dir_item {
+function is_valid_dir_item() {
+    local item=$1
     # Checks if valid item; not to match */. or */.. or /*
     [[ -f "$item" || -d "$item" ]] && ! [[ "$item" =~ \.\.?$|\*$ ]]
 }
 
 # Function to create symlinks for files in a directory
-function create_symlinks_in_dir {
+function create_symlinks_in_dir() {
     local directory=$1
     local destination=$2
 
@@ -69,33 +70,43 @@ function create_symlinks_in_dir {
 }
 
 # Function to print section titles and separators
-function print_section {
+function print_section() {
     local title=$1
     echo "-------------------"
     echo "$title"
     echo "-------------------"
 }
 
-print_section "Creating symbolic links for dotfiles"
-create_symlink "$DOTFILES_DIR/.profile" "$HOME/.profile"
-create_symlink "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
-create_symlinks_in_dir "$DOTFILES_DIR/zsh" "$HOME"
+function setup_shell() {
+    print_section "Creating symbolic links for dotfiles"
+    create_symlink "$DOTFILES_DIR/.profile" "$HOME/.profile"
+    create_symlink "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
+    create_symlinks_in_dir "$DOTFILES_DIR/zsh" "$HOME"
+}
 
-print_section "Checking Homebrew installation"
-if ! command -v brew &>/dev/null; then
-    echo "Homebrew not found. Installing..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-    echo "Homebrew found. Skipping installation."
-fi
+function setup_package_manager() {
+    print_section "Checking Homebrew installation"
+    if ! command -v brew &>/dev/null; then
+        echo "Homebrew not found. Installing..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+        echo "Homebrew found. Skipping installation."
+    fi
+    print_section "Installing Homebrew packages"
+    brew bundle --file "$DOTFILES_DIR/Brewfile"
+}
 
-print_section "Installing Homebrew packages"
-brew bundle --file "$DOTFILES_DIR/Brewfile"
+function setup_configs() {
+    print_section "Creating symbolic links for other configurations"
+    create_symlink "$DOTFILES_DIR/.config" "$HOME/.config"
+    create_symlink "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
+    create_symlinks_in_dir "$DOTFILES_DIR/vscode" "$HOME/Library/Application Support/Code/User"
+}
 
-print_section "Creating symbolic links for other configurations"
-create_symlink "$DOTFILES_DIR/.config" "$HOME/.config"
-create_symlink "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
-create_symlinks_in_dir "$DOTFILES_DIR/vscode" "$HOME/Library/Application Support/Code/User"
+function main() {
+    setup_shell
+    setup_package_manager
+    setup_configs
+}
 
-print_section "Installing Typescript"
-pnpm install --global typescript
+main
